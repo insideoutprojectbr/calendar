7ff6d0c52f9e4f43050b61e597235f94ef01f9d5
Dayany Espindola <dayanyrec@gmail.com>
2016-06-10 14:17:13 +0530

    Extracted events to json file

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,5 @@
 $(document).ready(function() {
-  $('#calendar').fullCalendar({
-    events: [
-      {
-        title  : 'event1',
-        start  : '2016-06-01'
-      },
-      {
-        title  : 'event2',
-        start  : '2016-06-05',
-        end    : '2016-06-07'
-      },
-      {
-        title  : 'event3',
-        start  : '2016-06-09T12:30:00',
-        allDay : false // will make the time show
-      }
-    ]
-  });
+    $('#calendar').fullCalendar({
+      events: '../events.json'
+    });
 });
